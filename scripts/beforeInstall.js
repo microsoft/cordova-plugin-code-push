3b99f7a3af96dade9d15c5b007bf9b1db14b0b48
Anna Kocheshkova <anna.kocheshkova@akvelon.com>
2018-01-08 12:57:49 +0300

    Added support for phonegap cli.Wrapped some functions in try..catch blocks.

--- scripts/beforeInstall.js
+++ scripts/beforeInstall.js
@@ -1,28 +1,46 @@
 ﻿module.exports = function (ctx) {
-	var exec = require('child_process').execSync;
+	var execSync = require('child_process').execSync;
 	var Q = ctx.requireCordovaModule('q');
 	var deferral = new Q.defer();
 
-	console.log("Detecting cordova version...");
-	var cordovaV = ctx.opts.cordova.version;
-	console.log("Cordova version is " + cordovaV);
+	var cordovaCLI = "cordova";
+	try {
+		execSync(cordovaCLI);
+	} catch (e) {
+		try {
+			cordovaCLI = "phonegap";
+			execSync(cordovaCLI);
+		} catch (e) {
+			throw new Error("An error occured. Please ensure that either the Cordova or PhoneGap CLI is installed.");
+			deferral.reject();
+		}
+	}
 
-	var plugins = exec('cordova plugin ls');
+	console.log("Detecting cordova version...");
+	var cordovaVersion = ctx.opts.cordova.version;
+	console.log("Cordova version is " + cordovaVersion);
+
+	var plugins = execSync(cordovaCLI + ' plugin ls');
 	if (!plugins.includes('cordova-plugin-file-transfer')) {
-		if (parseFloat(cordovaV) < 6.3) {
+		if (parseFloat(cordovaVersion) < 6.3) {
 			if (!plugins.includes('cordova-plugin-file')) {
 				console.log("Installing the compatible version of file plugin... ");
-				exec('cordova plugin add cordova-plugin-file@3.0.0');
+				execSync(cordovaCLI + ' plugin add cordova-plugin-file@3.0.0');
 			}
 			console.log("Installing the compatible version of file transfer plugin... ");
-			exec('cordova plugin add cordova-plugin-file-transfer@1.4.0');
+			execSync(cordovaCLI + ' plugin add cordova-plugin-file-transfer@1.4.0');
 		} else {
-			console.log("Installing the latest version of file-transfer plugin... ");
-			exec('cordova plugin add cordova-plugin-file-transfer@latest');
+			try {
+				console.log("Installing the latest version of file-transfer plugin... ");
+				execSync(cordovaCLI + ' plugin add cordova-plugin-file-transfer@latest');
+			} catch (e) {
+				console.log('The version of file plugin appears to be incompatible with the latest file transfer version. Installing compatible version of file transfer.... ');
+				execSync(cordovaCLI + ' plugin add cordova-plugin-file-transfer@1.4.0');
+			}
 		}
 	}
 	if (!plugins.includes('cordova-plugin-zip')) {
-		exec('cordova plugin add cordova-plugin-zip');
+		execSync(cordovaCLI + ' plugin add cordova-plugin-zip');
 	}
 	deferral.resolve();
 
