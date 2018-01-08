50589f3cc6925f2407afce4df792220171680c97
Anna Kocheshkova <anna.kocheshkova@akvelon.com>
2018-01-08 11:33:16 +0300

    Replaced exec method with sync version and added more output to the script.

--- scripts/beforeInstall.js
+++ scripts/beforeInstall.js
@@ -1,17 +1,27 @@
 module.exports = function(ctx) {
-    var exec = require('child_process').exec;
+    var exec = require('child_process').execSync;
     var Q = ctx.requireCordovaModule('q');
     var deferral = new Q.defer();
-    
-    console.log("Detecting cordova version...");
-    var cordovaV = ctx.opts.cordova.version;
 
+    console.log("Detecting cordova version...");
+    var cordovaV = ctx.opts.cordova.version;    
     console.log("Cordova version is " + cordovaV);
-    console.log("Removing file plugin to ensure cordova compatibility...");
-    exec('cordova plugin remove cordova-plugin-file');
+
+    var plugins = exec('cordova plugin ls');
+    if (plugins.includes('cordova-plugin-file-transfer')) {
+        console.log("Removing file transfer plugin to ensure cordova compatibility... ");    
+        exec('cordova plugin remove cordova-plugin-file-transfer');
+    }
+    plugins = exec('cordova plugin ls');
+    if (plugins.includes('cordova-plugin-file')) {
+        console.log("Removing file plugin to ensure cordova compatibility... ");
+        exec('cordova plugin remove cordova-plugin-file');
+    }
     if (parseFloat(cordovaV) < 6.3) {
+        console.log("Installing the compatible version of file-transfer plugin... ");
         exec('cordova plugin add cordova-plugin-file-transfer@1.6.3');
     } else {
+        console.log("Installing the latest version of file-transfer plugin... ");
         exec('cordova plugin add cordova-plugin-file-transfer@latest');
     }
     deferral.resolve();
