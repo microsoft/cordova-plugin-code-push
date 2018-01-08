c0ae01c5dcd40a726a8d65c2327733f8d9af036a
annaoomph <anchouskochesh@mail.ru>
2018-01-08 12:06:03 +0300

    Fixed logic: no need to install anything if plugin is present.
    Fixed versions of compatible plugins.

--- scripts/beforeInstall.js
+++ scripts/beforeInstall.js
@@ -1,4 +1,4 @@
-module.exports = function(ctx) {
+﻿module.exports = function(ctx) {
     var exec = require('child_process').execSync;
     var Q = ctx.requireCordovaModule('q');
     var deferral = new Q.defer();
@@ -8,22 +8,22 @@ module.exports = function(ctx) {
     console.log("Cordova version is " + cordovaV);
 
     var plugins = exec('cordova plugin ls');
-    if (plugins.includes('cordova-plugin-file-transfer')) {
-        console.log("Removing file transfer plugin to ensure cordova compatibility... ");    
-        exec('cordova plugin remove cordova-plugin-file-transfer');
-    }
-    plugins = exec('cordova plugin ls');
-    if (plugins.includes('cordova-plugin-file')) {
-        console.log("Removing file plugin to ensure cordova compatibility... ");
-        exec('cordova plugin remove cordova-plugin-file');
-    }
-    if (parseFloat(cordovaV) < 6.3) {
-        console.log("Installing the compatible version of file-transfer plugin... ");
-        exec('cordova plugin add cordova-plugin-file-transfer@1.6.3');
-    } else {
-        console.log("Installing the latest version of file-transfer plugin... ");
-        exec('cordova plugin add cordova-plugin-file-transfer@latest');
-    }
+    if (!plugins.includes('cordova-plugin-file-transfer')) {
+    	if (parseFloat(cordovaV) < 6.3) {
+			if (!plugins.includes('cordova-plugin-file')) {
+				console.log("Installing the compatible version of file plugin... ");
+				exec('cordova plugin add cordova-plugin-file@3.0.0');
+			}
+        	console.log("Installing the compatible version of file transfer plugin... ");		
+        	exec('cordova plugin add cordova-plugin-file-transfer@1.4.0');
+    	} else {
+        	console.log("Installing the latest version of file-transfer plugin... ");
+        	exec('cordova plugin add cordova-plugin-file-transfer@latest');	
+    	}
+	}
+	if (!plugins.includes('cordova-plugin-zip')) {
+		exec('cordova plugin add cordova-plugin-zip');	
+	}
     deferral.resolve();
 
     return deferral.promise;
