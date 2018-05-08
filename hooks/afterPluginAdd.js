1d100489de019df3234f0ad9cbc8c5bc5a8ec6d9
quintonn <q10athome@gmail.com>
2018-05-08 20:41:15 +0300

    Update file for backward compatibility
    Updated hook to be backward compatible with older version of nodejs

--- hooks/afterPluginAdd.js
+++ hooks/afterPluginAdd.js
@@ -1,4 +1,5 @@
 module.exports = function (ctx) {
+
     if (ctx.cmdLine.includes("cordova-plugin-code-push")) {
         console.log("Starting hook afterPluginAdd.js of corodva-plugin-code-push...");
         var execSync = require('child_process').execSync;
@@ -18,19 +19,19 @@ module.exports = function (ctx) {
         
         var plugins = ctx.opts.cordova.plugins;
 
-        if (!plugins.includes("cordova-plugin-file")) {
+		if (plugins.indexOf("cordova-plugin-file") == -1) {
             console.log("Adding the cordova-plugin-file@4.3.3... ");
             var output = execSync(cordovaCLI + ' plugin add cordova-plugin-file@4.3.3').toString();
             console.log(output);
         }
 
-        if (!plugins.includes("cordova-plugin-file-transfer")) {
+        if (plugins.indexOf("cordova-plugin-file-transfer") == -1) {
             console.log("Adding the cordova-plugin-file-transfer@1.6.3... ");
             var output = execSync(cordovaCLI + ' plugin add cordova-plugin-file-transfer@1.6.3').toString();
             console.log(output);
         }
 
-        if (!plugins.includes("cordova-plugin-zip")) {
+		if (plugins.indexOf("cordova-plugin-zip") == -1) {
             console.log("Adding the cordova-plugin-zip@3.1.0... ");
             var output = execSync(cordovaCLI + ' plugin add cordova-plugin-zip@3.1.0').toString();
             console.log(output);
