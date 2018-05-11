74e5ae1948552b5e0bfe9025e7cf8813bd5305b5
quintonn <q10athome@gmail.com>
2018-05-11 15:01:43 +0300

    Remove whitespace
    Removed tabbed white spaces

--- hooks/afterPluginAdd.js
+++ hooks/afterPluginAdd.js
@@ -18,7 +18,7 @@ module.exports = function (ctx) {
         
         var plugins = ctx.opts.cordova.plugins;
 
-		if (plugins.indexOf("cordova-plugin-file") == -1) {
+	if (plugins.indexOf("cordova-plugin-file") == -1) {
             console.log("Adding the cordova-plugin-file@4.3.3... ");
             var output = execSync(cordovaCLI + ' plugin add cordova-plugin-file@4.3.3').toString();
             console.log(output);
@@ -30,7 +30,7 @@ module.exports = function (ctx) {
             console.log(output);
         }
 
-		if (plugins.indexOf("cordova-plugin-zip") == -1) {
+	if (plugins.indexOf("cordova-plugin-zip") == -1) {
             console.log("Adding the cordova-plugin-zip@3.1.0... ");
             var output = execSync(cordovaCLI + ' plugin add cordova-plugin-zip@3.1.0').toString();
             console.log(output);
