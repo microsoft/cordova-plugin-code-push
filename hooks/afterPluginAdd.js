5dbb187d6161be739192bce5239ddaa74a2db702
alexander.goncharov <alexander.goncharov@akvelon.com>
2018-05-14 18:30:59 +0300

    * Fix issue with installing dependency in case if add plugin just in config.xml * Prevent duplicated installing of dependency plugins.

--- hooks/afterPluginAdd.js
+++ hooks/afterPluginAdd.js
@@ -1,41 +1,37 @@
 module.exports = function (ctx) {
-    if (ctx.cmdLine.indexOf("cordova-plugin-code-push") > -1 ) {
-        console.log("Starting hook afterPluginAdd.js of corodva-plugin-code-push...");
-        var execSync = require('child_process').execSync;
+    var execSync = require('child_process').execSync;
 
-        var cordovaCLI = "cordova";
+    var cordovaCLI = "cordova";
+    try {
+        execSync(cordovaCLI);
+    } catch (e) {
         try {
+            cordovaCLI = "phonegap";
             execSync(cordovaCLI);
         } catch (e) {
-            try {
-                cordovaCLI = "phonegap";
-                execSync(cordovaCLI);
-            } catch (e) {
-                deferral.reject("An error occured. Please ensure that either the Cordova or PhoneGap CLI is installed.");
-            }
+            deferral.reject("An error occured. Please ensure that either the Cordova or PhoneGap CLI is installed.");
         }
-        console.log("Using " + cordovaCLI + " CLI for adding plugins...");
-        
-        var plugins = ctx.opts.cordova.plugins;
+    }
 
-        if (plugins.indexOf("cordova-plugin-file") == -1) {
-            console.log("Adding the cordova-plugin-file@4.3.3... ");
-            var output = execSync(cordovaCLI + ' plugin add cordova-plugin-file@4.3.3').toString();
-            console.log(output);
-        }
+    var plugins = ctx.opts.cordova.plugins;
 
-        if (plugins.indexOf("cordova-plugin-file-transfer") == -1) {
-            console.log("Adding the cordova-plugin-file-transfer@1.6.3... ");
-            var output = execSync(cordovaCLI + ' plugin add cordova-plugin-file-transfer@1.6.3').toString();
-            console.log(output);
-        }
+    if (plugins.indexOf("cordova-plugin-file") == -1) {
+        console.log("Adding the cordova-plugin-file@4.3.3... ");
+        var output = execSync(cordovaCLI + ' plugin add cordova-plugin-file@4.3.3').toString();
+        console.log(output);
+        plugins = execSync('cordova plugin').toString();
+    }
 
-        if (plugins.indexOf("cordova-plugin-zip") == -1) {
-            console.log("Adding the cordova-plugin-zip@3.1.0... ");
-            var output = execSync(cordovaCLI + ' plugin add cordova-plugin-zip@3.1.0').toString();
-            console.log(output);
-        }
+    if (plugins.indexOf("cordova-plugin-file-transfer") == -1) {
+        console.log("Adding the cordova-plugin-file-transfer@1.6.3... ");
+        var output = execSync(cordovaCLI + ' plugin add cordova-plugin-file-transfer@1.6.3').toString();
+        console.log(output);
+        plugins = execSync('cordova plugin').toString();
+    }
 
-        console.log("Finished hook afterPluginAdd.js of corodva-plugin-code-push.");
+    if (plugins.indexOf("cordova-plugin-zip") == -1) {
+        console.log("Adding the cordova-plugin-zip@3.1.0... ");
+        var output = execSync(cordovaCLI + ' plugin add cordova-plugin-zip@3.1.0').toString();
+        console.log(output);
     }
 };
