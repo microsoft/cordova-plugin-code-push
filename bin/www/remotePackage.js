c32af4d46501b99b04739b3e767040c76ef595c2
Igor Krasavin <v-igkras@microsoft.com>
2020-06-03 12:48:48 +0300

    Update bin

--- bin/www/remotePackage.js
+++ bin/www/remotePackage.js
@@ -72,11 +72,11 @@ var RemotePackage = (function (_super) {
                         downloadProgress(dp);
                     }
                 };
-                this.currentFileTransfer.download(this.downloadUrl, cordova.file.dataDirectory + LocalPackage.DownloadDir + "/" + LocalPackage.PackageUpdateFileName, downloadSuccess, downloadError, true);
+                this.currentFileTransfer.download(this.downloadUrl, cordova.file.dataDirectory + LocalPackage.DownloadDir + "/" + LocalPackage.PackageUpdateFileName, downloadSuccess, downloadError);
             }
         }
         catch (e) {
-            CodePushUtil.invokeErrorCallback(new Error("An error occured while downloading the package. " + (e && e.message) ? e.message : ""), errorCallback);
+            CodePushUtil.invokeErrorCallback(new Error("An error occurred while downloading the package. " + (e && e.message) ? e.message : ""), errorCallback);
         }
     };
     RemotePackage.prototype.abortDownload = function (abortSuccess, abortError) {
